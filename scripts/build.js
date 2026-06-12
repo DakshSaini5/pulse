8b1b2ce4d7c67bab74532e84c2a187911fc69b76
DakshSaini5 <dakshhh98@gmail.com>
2026-06-13 03:35:58 +0530

    Revert "Implement dynamic monorepo build routing for Vercel and Railway"
    This reverts commit 40973ba80b6a31044043f47ad74c8a505461eabd.

--- scripts/build.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-
-if (process.env.VERCEL === '1') {
-  console.log('--- VERCEL ENVIRONMENT DETECTED: BUILDING FRONTEND ---');
-  execSync('cd frontend && npm run build', { stdio: 'inherit' });
-} else {
-  console.log('--- DEFAULT/RAILWAY ENVIRONMENT DETECTED: BUILDING BACKEND ---');
-  execSync('cd backend && npm run build', { stdio: 'inherit' });
-}
--- scripts/install.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-
-if (process.env.VERCEL === '1') {
-  console.log('--- VERCEL ENVIRONMENT DETECTED: INSTALLING FRONTEND ---');
-  execSync('cd frontend && npm install', { stdio: 'inherit' });
-} else {
-  console.log('--- DEFAULT/RAILWAY ENVIRONMENT DETECTED: INSTALLING BACKEND ---');
-  execSync('cd backend && npm install', { stdio: 'inherit' });
-}
